29f5274d82625c7b1bdd7efa0d0ab10a503a7813
TITOTTO Franck <114482795+TITOTTO@users.noreply.github.com>
2023-05-03 12:00:23 +0200

    FTI - Devise + loggin screen (#1)
    * FTI - Devise + loggin screen

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_123649) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_125150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "level", default: 0
+    t.boolean "deleted", default: false, null: false
+    t.boolean "deleted_by_admin", default: false, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "pseudo"
+    t.string "phone_number1"
+    t.string "phone_number2"
+    t.date "last_activity_on"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
